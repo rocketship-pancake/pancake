adbbb09625e449123783d7d35bb68a88643f234f
rocketship-pancake <22814972+rocketship-pancake@users.noreply.github.com>
2024-12-01 09:57:10 +0200

    Update mullvad-vpn.sh

--- files/scripts/mullvad-vpn.sh
+++ files/scripts/mullvad-vpn.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -oue pipefail
-sudo tee -a /etc/yum.repos.d/mullvad.repo << 'EOF'
-[mullvad-beta]
-name=Mullvad VPN (beta)
-baseurl=https://repository.mullvad.net/rpm/beta/$basearch
-type=rpm
-enabled=1
-gpgcheck=1
-gpgkey=https://repository.mullvad.net/rpm/mullvad-keyring.asc
-EOF
+
+wget --content-disposition https://mullvad.net/download/app/rpm/latest
+rpm-ostree install MullvadVPN-*.rpm
+rm -f MullvadVPN-*.rpm
