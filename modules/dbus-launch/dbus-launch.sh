9d3618988e4d262006c8346afc64338b1836ea53
rocketship-pancake <22814972+rocketship-pancake@users.noreply.github.com>
2024-12-03 10:23:18 +0200

    Update recipe.yml

--- modules/dbus-launch/dbus-launch.sh
+++ modules/dbus-launch/dbus-launch.sh
@@ -2,10 +2,5 @@
 
 set -oue pipefail
 
-dbus-uuidgen > /var/lib/dbus/machine-id
-
-mkdir -p /var/run/dbus
-dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
-
 sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-theme 'Breeze'
 sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-size 32
