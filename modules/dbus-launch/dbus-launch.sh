2b71c1a9e52535f88be8e3f111f4a3eb1615ab99
rocketship-pancake <22814972+rocketship-pancake@users.noreply.github.com>
2024-12-03 10:09:14 +0200

    weird stuff

--- modules/dbus-launch/dbus-launch.sh
+++ modules/dbus-launch/dbus-launch.sh
@@ -2,5 +2,10 @@
 
 set -oue pipefail
 
+dbus-uuidgen > /var/lib/dbus/machine-id
+
+mkdir -p /var/run/dbus
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+
 sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-theme 'Breeze'
 sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-size 32
