9c64de3ef412d88d9bf8da2742c6e92f1a378612
rocketship-pancake <22814972+rocketship-pancake@users.noreply.github.com>
2024-12-03 13:42:37 +0200

    Update recipe.yml

--- files/scripts/gdm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-theme 'Breeze'
-sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-size 32
