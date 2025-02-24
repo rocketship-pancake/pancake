03a22ac763ab97d63dd65ced3b7a39dda11838ee
rocketship-pancake <22814972+rocketship-pancake@users.noreply.github.com>
2025-02-24 18:36:12 +0200

    add rclone script

---
+++ files/scripts/rclone-beta.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Download the install script
+curl https://rclone.org/install.sh > install-rclone.sh
+
+# Run the install script
+sudo -v && sudo bash install-rclone.sh beta
+
+# Install Python keyring
+pip install --user keyring
+
+# Download the management script
+curl https://gist.githubusercontent.com/oleduc/b3473b34801f9618b77e579392a12d79/raw/3fa46a7adc9418d59cd0e38b096765e919fc703d/rclone-mount-proton.sh > rclone-mount-proton.sh
+sudo mv rclone-mount-proton.sh /usr/local/bin/rclone-mount-proton.sh
+
+# Give the script execution permissions
+sudo chmod +x /usr/local/bin/rclone-mount-proton.sh
