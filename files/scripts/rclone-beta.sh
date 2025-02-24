3c5b7990150ef12d0d0b22e9bfaa784fe160052b
rocketship-pancake <22814972+rocketship-pancake@users.noreply.github.com>
2025-02-24 22:30:23 +0200

    add rclone-mount-proton.sh

--- files/scripts/rclone-beta.sh
+++ files/scripts/rclone-beta.sh
@@ -15,7 +15,7 @@ bash install-rclone.sh beta
 pip install --user keyring
 
 # Download the management script
-wget -P /usr/local/bin https://gist.githubusercontent.com/oleduc/b3473b34801f9618b77e579392a12d79/raw/3fa46a7adc9418d59cd0e38b096765e919fc703d/rclone-mount-proton.sh
+#wget -P /usr/local/bin https://gist.githubusercontent.com/oleduc/b3473b34801f9618b77e579392a12d79/raw/3fa46a7adc9418d59cd0e38b096765e919fc703d/rclone-mount-proton.sh
 
 # Give the script execution permissions
 chmod +x /usr/local/bin/rclone-mount-proton.sh
